ccaf254f2dfb66fc491382e2b867ce1cbdf14c0f
ningtianmin <ningtianmin@foxmail.com>
2018-03-27 13:44:41 +0800

    fix SortUtil.isIncrementalSequence

--- src/main/java/sort/SortUtil.java
+++ src/main/java/sort/SortUtil.java
@@ -5,6 +5,9 @@ package sort;
  */
 public class SortUtil {
     public static <T extends Comparable<? super T>> boolean isIncrementalSequence(T[] array) {
+        if (array == null) {
+            return true;
+        }
         for (int i = 0, length = array.length; i < length - 1; i++) {
             if (array[i].compareTo(array[i + 1]) > 0) {
                 return false;
