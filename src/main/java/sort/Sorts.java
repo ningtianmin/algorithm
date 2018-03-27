788ca688fb441185ffb0bbb185ab7559d6668057
ningtianmin <ningtianmin@foxmail.com>
2018-03-27 13:43:55 +0800

    add quickSort2Way and quickSort3Way

--- src/main/java/sort/Sorts.java
+++ src/main/java/sort/Sorts.java
@@ -86,4 +86,92 @@ public class Sorts {
             System.arraycopy(tempArray, j, array, p, rightIndex - p + 1);
         }
     }
+
+    public static <T extends Comparable<? super T>> T[] quickSort2Way(T[] array) {
+        System.out.println(String.format("quickSort3Way begin | array = %s", Arrays.toString(array)));
+        if (array == null) {
+            System.out.println("quickSort3Way end | array = null\n");
+            return null;
+        }
+        quickSort2Way(array, 0, array.length - 1);
+        System.out.println(String.format("quickSort3Way end | array = %s\n", Arrays.toString(array)));
+        return array;
+    }
+
+    private static <T extends Comparable<? super T>> void quickSort2Way(T[] array, int start, int end) {
+        if (array == null || start < 0 || end > array.length - 1 || start >= end) {
+            return;
+        }
+        T pivot = array[start];
+        int left = start, right = end;
+        while (left < right) {
+            while (left < right && array[right].compareTo(pivot) >= 0) {
+                right--;
+            }
+            while (left < right && array[left].compareTo(pivot) <= 0) {
+                left++;
+            }
+            if (left < right) {
+                T temp = array[left];
+                array[left] = array[right];
+                array[right] = temp;
+            }
+        }
+        array[start] = array[left];
+        array[left] = pivot;
+        quickSort2Way(array, start, left - 1);
+        quickSort2Way(array, left + 1, end);
+    }
+
+    public static <T extends Comparable<? super T>> T[] quickSort3Way(T[] array) {
+        System.out.println(String.format("quickSort3Way begin | array = %s", Arrays.toString(array)));
+        if (array == null) {
+            System.out.println("quickSort3Way end | array = null\n");
+            return null;
+        }
+        quickSort3Way(array, 0, array.length - 1);
+        System.out.println(String.format("quickSort3Way end | array = %s\n", Arrays.toString(array)));
+        return array;
+    }
+
+    private static <T extends Comparable<? super T>> void quickSort3Way(T[] array, int start, int end) {
+        if (array == null || start < 0 || end > array.length - 1 || start >= end) {
+            return;
+        }
+        T pivot = array[start];
+        int left = start, right = end;
+        int leftEqual = left - 1, rightEqual = end + 1;
+        while (left < right) {
+            while (left < right && array[right].compareTo(pivot) >= 0) {
+                if (array[right].compareTo(pivot) == 0) {
+                    array[right] = array[--rightEqual];
+                    array[rightEqual] = pivot;
+                }
+                right--;
+            }
+            while (left < right && array[left].compareTo(pivot) <= 0) {
+                if (array[left].compareTo(pivot) == 0) {
+                    array[left] = array[++leftEqual];
+                    array[leftEqual] = pivot;
+                }
+                left++;
+            }
+            if (left < right) {
+                T temp = array[left];
+                array[left] = array[right];
+                array[right] = temp;
+            }
+        }
+        while (leftEqual >= start) {
+            array[leftEqual--] = array[left];
+            array[left--] = pivot;
+        }
+        right++;
+        while (rightEqual <= end) {
+            array[rightEqual++] = array[right];
+            array[right++] = pivot;
+        }
+        quickSort3Way(array, start, left);
+        quickSort3Way(array, right, end);
+    }
 }
--- src/test/java/sort/SortsTest.java
+++ src/test/java/sort/SortsTest.java
@@ -38,4 +38,30 @@ class SortsTest {
         System.out.println(String.format("mergeSort:%s", Arrays.toString(array)));
         Assertions.assertTrue(SortUtil.isIncrementalSequence(array));
     }
+
+    @Test
+    void quickSort2Way() {
+        Integer[] array;
+        array = null;
+        Assertions.assertTrue(SortUtil.isIncrementalSequence(Sorts.quickSort2Way(array)));
+        array = new Integer[]{1};
+        Assertions.assertTrue(SortUtil.isIncrementalSequence(Sorts.quickSort2Way(array)));
+        array = new Integer[]{1, 1};
+        Assertions.assertTrue(SortUtil.isIncrementalSequence(Sorts.quickSort2Way(array)));
+        array = new Integer[]{5, 4, 4, 2, 1};
+        Assertions.assertTrue(SortUtil.isIncrementalSequence(Sorts.quickSort2Way(array)));
+    }
+
+    @Test
+    void quickSort3Way() {
+        Integer[] array;
+        array = null;
+        Assertions.assertTrue(SortUtil.isIncrementalSequence(Sorts.quickSort3Way(array)));
+        array = new Integer[]{1};
+        Assertions.assertTrue(SortUtil.isIncrementalSequence(Sorts.quickSort3Way(array)));
+        array = new Integer[]{1, 1};
+        Assertions.assertTrue(SortUtil.isIncrementalSequence(Sorts.quickSort3Way(array)));
+        array = new Integer[]{1, 3, 4, 2, 2, 1, 4, 5, 6};
+        Assertions.assertTrue(SortUtil.isIncrementalSequence(Sorts.quickSort3Way(array)));
+    }
 }
